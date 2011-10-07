0ecd2ec281b0e15d9ce25a03b73a167486da09ec
Morgan Wahl <morgy.wahl@gmail.com>
2011-10-07 13:43:06 -0400

    added shell script to install the thumbnailers in gnome

---
+++ install_thumbnailers.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+for mime in "application@x-mood"
+do
+  gconftool-2 -s -t string "/desktop/gnome/thumbnailers/${mime}/command" "$HOME/bin/mood_thumbnailer.py -s %s -o %o %i"
+  gconftool-2 -s -t boolean "/desktop/gnome/thumbnailers/${mime}/enable" True
+done
+
+for mime in {"audio@flac","audio@x-wav","audio@mp4","audio@mpeg","audio@x-vorbis@ogg"}
+do
+  gconftool-2 -s -t string "/desktop/gnome/thumbnailers/${mime}/command" "$HOME/bin/audio_thumbnailer.py -s %s -o %o %i"
+  gconftool-2 -s -t boolean "/desktop/gnome/thumbnailers/${mime}/enable" True
+done
+
